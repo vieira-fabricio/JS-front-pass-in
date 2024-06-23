82ec4a756c5233e2b45dae4725a860f393424aff
fabriciovieira <Bibito23@>
2024-06-23 15:41:05 -0300

    adicionado telapara criar um novo evento

---
+++ novo-evento/script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const eventForm = document.getElementById('eventForm');
+
+    eventForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o comportamento padrão de enviar o formulário
+
+        // Obter os valores dos inputs
+        const title = document.getElementById('title').value;
+        const description = document.getElementById('description').value;
+        const capacity = parseInt(document.getElementById('capacity').value);
+
+        // Exemplo de manipulação dos dados (exibindo no console)
+        console.log('Título do Evento:', title);
+        console.log('Descrição:', description);
+        console.log('Capacidade de Pessoas:', capacity);
+
+        // Aqui você pode adicionar mais lógica para enviar os dados para um servidor, por exemplo
+        // Exemplo:
+        createEvent(title, description, capacity);
+    });
+
+    async function createEvent(title, description, capacity) {
+        
+        const apiUrl = 'http://localhost:8080/events';
+
+        const data = {
+            title: title,
+            details: description,
+            maximumAttendees: capacity
+        };
+
+        try {
+            const response = await axios.post(apiUrl, data);
+
+            // Exibe mensagem de sucesso
+            showSuccessMessage();
+        } catch (error) {
+            console.error('Erro ao enviar dados para a api!', error);
+
+            // Mostra o erro na tela para depuração
+            const errorElement = document.getElementById('errorMessage');
+            errorElement.textContent = 'Erro ao criar evento. Detalhes: ' + error.message;
+            errorElement.style.display = 'block'; // Exibe a mensagem
+        }
+    }
+
+    function showSuccessMessage() {
+
+        const sucessMessage = document.getElementById('successMessage')
+        sucessMessage.style.display = 'block'; // Exibe a mensagem
+
+        // Esconde a mensagem após 3 segundos (3000 milissegundos)
+        setTimeout(function(){
+            sucessMessage.style.display = 'none'; // Esconde a mensagem após 3 segundos
+        }, 3000);
+    }
+});
+
+document.getElementById('btnVoltar').addEventListener('click', function() {
+    // Redirecionar para a página de criar novo evento
+    window.location.href = '../lista-eventos/listaEvento.html';
+});
